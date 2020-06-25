b12631b07890985bf8b2e9279bd55ce2e8d0b6e9
GuilhermeNovaes254 <guilherme.novaes@gmail.com>
2020-06-25 20:04:56 -0300

    Conversion is OK, MVP Ready

--- assets/script.js
+++ assets/script.js
@@ -3,29 +3,28 @@
 function copyDecimal() {
 
     let copyText = document.getElementById("decimalForm");
-    return copyText;
+    copy(copyText)
 }
 
 function copyHexa() {
 
     let copyText = document.getElementById("hexadecimalForm");
-    hexaDec(copyText.value)
-    return copyText;
+    copy(copyText)
 }
 
 function copyBin() {
 
     let copyText = document.getElementById("binForm");
-    return copyText;
+    copy(copyText)
 }
 
 function copyOcta() {
 
     let copyText = document.getElementById("octalForm");
-    return copyText;
+    copy(copyText)
 }
 
-function copy(copyText){
+function copy(copyText) {
     copyText.prevent
     copyText.select();
     copyText.setSelectionRange(0, 99999)
@@ -34,33 +33,67 @@ function copy(copyText){
 
 // Conversion
 //Decimal - To...
-function decaHex(decimal){
-    return decimal.toString(16)
+function decaHex(decimal) {
+    return parseInt(decimal, 10).toString(16)
 }
 
-function decaBin(decimal){
-    return decimal.toString(2)
+function decaBin(decimal) {
+    return parseInt(decimal, 10).toString(2)
 }
 
-function decaOct(decimal){
-    return decimal.toString(8)
+function decaOct(decimal) {
+    return parseInt(decimal, 10).toString(8)
 }
 
 //...To -  Decimal
 
-function hexaDec(hexadecimal){
-    return parseInt(hexadecimal, 16);
+function hexaDec(hexa) {
+    return parseInt(hexa, 16);
 }
 
-function binDec(binary){
+function binDec(binary) {
     return parseInt(binary, 2);
 }
 
-function octDec(octal){
+function octDec(octal) {
     return parseInt(octal, 8);
 }
-let id  = 0
-document.addEventListener('keyup',  function() {
-    id++
-    console.log(id)
+
+let dec = document.getElementById("decimalForm");
+let hexa = document.getElementById("hexadecimalForm");
+let bin = document.getElementById("binForm");
+let octal = document.getElementById("octalForm");
+
+document.getElementById("decimalForm").addEventListener('change', function () {
+//OK
+    hexa.value = decaHex(dec.value).toUpperCase();
+    bin.value =  decaBin(dec.value);
+    octal.value =  decaOct(dec.value);
+
+});
+
+
+document.getElementById("hexadecimalForm").addEventListener('change', function () {
+//Ok
+   
+    dec.value = hexaDec(hexa.value);
+    bin.value =  decaBin(dec.value);
+    octal.value =  decaOct(dec.value);
+});
+
+
+
+document.getElementById("binForm").addEventListener('change', function () {
+
+    dec.value = binDec(bin.value);
+    hexa.value = decaHex(dec.value).toUpperCase();
+    octal.value =  decaOct(dec.value);
+});
+
+
+document.getElementById("octalForm").addEventListener('change', function () {
+
+    dec.value = octDec(octal.value);
+    hexa.value = decaHex(dec.value).toUpperCase();
+    bin.value =  decaBin(dec.value);
 });
